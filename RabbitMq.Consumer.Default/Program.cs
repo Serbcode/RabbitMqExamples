32d73d2b58c2c30c60b4199f1dbf783dbf5b8210
Serge Bazunoff <sergey.bazunoff@gmail.com>
2022-04-19 17:52:10 +0300

    Example with Default Exchange

---
+++ RabbitMq.Consumer.Default/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMq.Consumer.Default
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {             
+                    channel.QueueDeclare("dev-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) => {
+                        var body = e.Body;
+                        var mes = Encoding.UTF8.GetString(body.ToArray());
+                        System.Console.WriteLine("Recieved message {0}", mes);
+                    };
+
+                    channel.BasicConsume("", autoAck: true, consumer: consumer);
+
+                    System.Console.WriteLine("Subscribed to the queue 'dev-gueue'... ");
+                }
+            }        
+        }
+    }
+}
---
+++ RabbitMq.Publisher.Default/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMq.Publisher.Default
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("dev-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    
+                    string message = "Mesasge from Publisher";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "dev-queue", basicProperties: null, 
+                    body: body);
+                    Console.WriteLine("Message was sent into Default Exchange");
+                }
+            }            
+        }
+    }
+}
